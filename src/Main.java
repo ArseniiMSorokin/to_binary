97a14b7f90364055dd0cd14b7a478188e1cf75be
ArseniiMSorokin <arseniimsorokin@mail.ru>
2024-09-18 17:16:55 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inp_num = sc.nextInt();
+        String out_num = "";
+        while (inp_num>0) {
+            out_num=inp_num%2 + out_num;
+            inp_num=inp_num/2;
+        }
+        System.out.println(out_num);
+    }
+}
